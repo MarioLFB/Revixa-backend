5a27f7976d99a63a2a6a3b67aab1b8ac03152de6
Mario Borges <popesco19@hotmail.com>
2024-10-11 16:13:56 +0100

    Add Post model to social app

--- social/models.py
+++ social/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Model representing a post in the social network.
+    """
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post by {self.author.username}"
