3e2852e359d7906794a86864f735d155ea66072b
Mario Borges <popesco19@hotmail.com>
2024-10-11 16:17:31 +0100

    Add URL patterns for Post and Like models in social app

---
+++ social/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostListCreateView, PostDetailView, LikePostView
+
+urlpatterns = [
+    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
+]
