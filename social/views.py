2f2b79812ccbaaf16abd0a2aada004c53be648be
Mario Borges <popesco19@hotmail.com>
2024-10-11 16:18:47 +0100

    Add PostListCreateView for creating and listing posts with authentication

--- social/views.py
+++ social/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Post, Like
+from .serializers import PostSerializer, LikeSerializer
 
-# Create your views here.
+
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user) 
