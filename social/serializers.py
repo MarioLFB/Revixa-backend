4ea56151c9570a95f42d8b7b60f274cef619661b
Mario Borges <popesco19@hotmail.com>
2024-10-11 16:16:30 +0100

    Add PostSerializer in social app with class meta

--- social/serializers.py
+++ social/serializers.py
@@ -1,2 +1,13 @@
 from rest_framework import serializers
 from .models import Post, Like
+
+class PostSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'author', 'created_at', 'updated_at', 'likes_count']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
