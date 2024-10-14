54499fdc411c28c05864d0df5fc887f26053cd00
Mario Borges <popesco19@hotmail.com>
2024-10-14 16:31:07 +0100

    Import MyTokenObtainPairSerializer and TokenObtainPairView

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, MyTokenObtainPairSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
