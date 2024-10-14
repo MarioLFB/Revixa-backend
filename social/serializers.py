58c4f5508214baf1578636ef9744af3d2a190468
Mario Borges <popesco19@hotmail.com>
2024-10-14 18:11:15 +0100

    Refactor PostSerializer to include 'liked_by' field

--- social/serializers.py
+++ social/serializers.py
@@ -4,13 +4,17 @@ from .models import Post, Like
 class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.SerializerMethodField()
     author = serializers.ReadOnlyField(source='author.username')
+    liked_by = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'content', 'author', 'created_at', 'updated_at', 'likes_count']
+        fields = ['id', 'content', 'author', 'created_at', 'updated_at', 'likes_count', 'liked_by']
 
     def get_likes_count(self, obj):
         return obj.likes.count()
+    
+    def get_liked_by(self, obj):
+        return [like.user.username for like in obj.likes.all()]
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
