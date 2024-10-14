fe015352c162450264869e2ab298828d0094d4ad
Mario Borges <popesco19@hotmail.com>
2024-10-14 16:22:55 +0100

    Fix serializers.py to import TokenObtainPairSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
+
 
 
 class UserSerializer(serializers.ModelSerializer):
