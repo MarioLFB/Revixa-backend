30d8a944dee04f7fbc39ac2eb8755a7a00f718b9
Mario Borges <popesco19@hotmail.com>
2024-10-15 18:02:28 +0100

    Add IsAdminUser permission class to check admin access.

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+class IsAdminUser(permissions.BasePermission):
+    """
+    Permission class to check if the user is an admin
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
