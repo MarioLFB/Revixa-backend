d13c0e2e404dcf1c9b425b02b218c35d139ff806
Mario Borges <popesco19@hotmail.com>
2024-10-15 18:08:49 +0100

    Add URL route for listing and creating posts related to specific reviews.

--- social/urls.py
+++ social/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostListCreateView, PostDetailView, LikePostView
 
 urlpatterns = [
-    path('posts/', PostListCreateView.as_view(), name='post-list-create'),
+    path('reviews/<int:review_id>/posts/', PostListCreateView.as_view(), name='review-post-list-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
 ]
