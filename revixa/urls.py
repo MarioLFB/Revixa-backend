c6c95acbb4a1d6965bd1f7904f592a3d7e0c6dc2
Mario Borges <popesco19@hotmail.com>
2024-10-15 18:04:49 +0100

    Update URL patterns to include user registration, JWT authentication, and reviews endpoints.

--- revixa/urls.py
+++ revixa/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.views import CreateUserView, MyTokenObtainPairView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +9,6 @@ urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('api-auth/', include("rest_framework.urls")),
-
     path('social/', include('social.urls')),
+    path('reviews/', include('reviews.urls')),
 ]
