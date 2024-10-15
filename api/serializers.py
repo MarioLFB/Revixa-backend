ed2b55dac49b7d34bd336e1bbf0b7cfa31a50354
Mario Borges <popesco19@hotmail.com>
2024-10-15 17:54:04 +0100

    Refactor MyTokenObtainPairSerializer to include 'is_staff' field in the token

--- api/serializers.py
+++ api/serializers.py
@@ -21,5 +21,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         token['username'] = user.username
+        token['is_staff'] = user.is_staff
 
-        return token   
+        return token
