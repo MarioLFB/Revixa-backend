1345a76f37a36c7e848569ad84453daea8a9e7a6
Mario Borges <popesco19@hotmail.com>
2024-10-29 18:43:15 +0000

    Add UserProfileView endpoint to urlpatterns

--- revixa/urls.py
+++ revixa/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView, MyTokenObtainPairView, UpdateEmailView, UpdatePasswordView
+from api.views import CreateUserView, MyTokenObtainPairView, UpdateEmailView, UpdatePasswordView, UserProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('api/user/update-email/', UpdateEmailView.as_view(), name="update_email"),
     path('api/user/update-password/', UpdatePasswordView.as_view(), name="update_password"),
+    path('api/user/profile/', UserProfileView.as_view(), name="user_profile"),
     path('api-auth/', include("rest_framework.urls")),
     path('social/', include('social.urls')),
     path('reviews/', include('reviews.urls')),
