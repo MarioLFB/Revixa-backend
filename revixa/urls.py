bc79ea684fd1098b358c808a07eb9eb41e9b20e9
Mario Borges <popesco19@hotmail.com>
2024-10-29 18:06:05 +0000

    add API endpoints for UpdateEmailView and UpdatePasswordView in urlpatterns.

--- revixa/urls.py
+++ revixa/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView, MyTokenObtainPairView
+from api.views import CreateUserView, MyTokenObtainPairView, UpdateEmailView, UpdatePasswordView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path('api/user/register/', CreateUserView.as_view(), name="register"),
     path('api/token/', MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/user/update-email/', UpdateEmailView.as_view(), name="update_email"),
+    path('api/user/update-password/', UpdatePasswordView.as_view(), name="update_password"),
     path('api-auth/', include("rest_framework.urls")),
     path('social/', include('social.urls')),
     path('reviews/', include('reviews.urls')),
