0c213d3100f8a393e87c2197df6a535afba215ca
Mario Borges <popesco19@hotmail.com>
2024-11-01 19:17:06 +0000

    Update CORS settings for local development; allow credentials

--- revixa/settings.py
+++ revixa/settings.py
@@ -73,10 +73,13 @@ if 'CLIENT_ORIGIN' in os.environ:
         os.environ.get('CLIENT_ORIGIN')
     ]
 else:
+    # Em desenvolvimento local
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^http://127\.0\.0\.1:5173$",
         r"^http://localhost:5173$",
     ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'revixa.urls'
 
