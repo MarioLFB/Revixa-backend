5f7ce389951853345d6b7d7080cb055a7b8aa208
Mario Borges <popesco19@hotmail.com>
2024-11-09 22:18:20 +0000

    Refactor ReviewAdmin list_display for improved readability

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Review
 
+
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('title', 'framework_name', 'framework_version', 'author', 'created_at')
+    list_display = (
+        'title', 'framework_name', 'framework_version', 'author', 'created_at'
+    )
     search_fields = ('title', 'framework_name', 'author__username')
     list_filter = ('framework_name', 'created_at')
     readonly_fields = ('created_at', 'updated_at')
+
 
 admin.site.register(Review, ReviewAdmin)
