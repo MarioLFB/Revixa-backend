5f0aef9b3c631996d8aa573d4379fd547040fb41
Mario Borges <popesco19@hotmail.com>
2024-11-09 23:14:20 +0000

    Add admin interfaces for Post and Like models with custom configurations

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Like
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'content', 'created_at', 'updated_at', 'review')
+    search_fields = ('content', 'author__username')
+    list_filter = ('created_at', 'updated_at', 'author')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'created_at')
+    search_fields = ('user__username', 'post__content')
+    list_filter = ('created_at', 'user')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
