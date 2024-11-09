a95238d9bd3b04825bf7b2f5602ee51caeb2e2f8
Mario Borges <popesco19@hotmail.com>
2024-11-09 22:25:10 +0000

    Refactor imports in urls.py for improved readability and consistency

--- revixa/urls.py
+++ revixa/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView, MyTokenObtainPairView, UpdateEmailView, UpdatePasswordView, UserProfileView
+from api.views import (
+    CreateUserView, MyTokenObtainPairView, UpdateEmailView, UpdatePasswordView,
+    UserProfileView
+)
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
