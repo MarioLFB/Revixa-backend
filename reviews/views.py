95ab55be7b769eec382f5c2e57f6436ae9957912
Mario Borges <popesco19@hotmail.com>
2024-11-09 22:21:38 +0000

    Clean up whitespace and formatting in views.py for improved readability

--- reviews/views.py
+++ reviews/views.py
@@ -2,12 +2,14 @@ from rest_framework import generics, permissions
 from .models import Review
 from .serializers import ReviewSerializer
 
+
 class IsAdminUser(permissions.BasePermission):
     """
     Permission class to check if the user is an admin
     """
     def has_permission(self, request, view):
         return request.user and request.user.is_staff
+
 
 class ReviewListCreateView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
@@ -20,6 +22,7 @@ class ReviewListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
 
 class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
