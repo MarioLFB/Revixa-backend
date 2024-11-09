c9682b2847d71ca3b39f3ae1c31fd0655e1b80c9
Mario Borges <popesco19@hotmail.com>
2024-11-09 22:42:49 +0000

    Refactor URL patterns in urls.py for improved readability and consistency

--- revixa/urls.py
+++ revixa/urls.py
@@ -8,12 +8,36 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/register/', CreateUserView.as_view(), name="register"),
-    path('api/token/', MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
-    path('api/user/update-email/', UpdateEmailView.as_view(), name="update_email"),
-    path('api/user/update-password/', UpdatePasswordView.as_view(), name="update_password"),
-    path('api/user/profile/', UserProfileView.as_view(), name="user_profile"),
+    path(
+        'api/user/register/',
+        CreateUserView.as_view(),
+        name="register"
+    ),
+    path(
+        'api/token/',
+        MyTokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        'api/token/refresh/',
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
+    path(
+        'api/user/update-email/',
+        UpdateEmailView.as_view(),
+        name="update_email"
+    ),
+    path(
+        'api/user/update-password/',
+        UpdatePasswordView.as_view(),
+        name="update_password"
+    ),
+    path(
+        'api/user/profile/',
+        UserProfileView.as_view(),
+        name="user_profile"
+    ),
     path('api-auth/', include("rest_framework.urls")),
     path('social/', include('social.urls')),
     path('reviews/', include('reviews.urls')),
