a35fb57cf6f68d21203bd168a2ae6af2937dc50c
Mario Borges <popesco19@hotmail.com>
2024-11-09 22:20:49 +0000

    Refactor ReviewSerializer fields for improved readability and consistency

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import Review
 from social.serializers import PostSerializer
 
+
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     posts = PostSerializer(many=True, read_only=True)
 
     class Meta:
         model = Review
-        fields = ['id', 'title', 'content', 'framework_name', 'framework_version', 'author', 'created_at', 'updated_at', 'posts']
+        fields = [
+            'id', 'title', 'content', 'framework_name', 'framework_version',
+            'author', 'created_at', 'updated_at', 'posts'
+        ]
